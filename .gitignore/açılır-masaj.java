e621eb152d9c2d8b80210f49c8694cd63df13941
Java2008 <50770385+Java2008@users.noreply.github.com>
2019-06-07 18:16:49 +0300

    Create açılır-masaj.java

---
+++ .gitignore/açılır-masaj.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class açılırmesaj {
+
+	public static void main(String[]args) {
+		
+		JOptionPane.showMessageDialog(null,"Bu Bir Uyarı Mesajıdır");
+	}
+	
+}
