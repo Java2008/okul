4bd2b4fe008e69fd485a8805f89b368f5d483b14
Java2008 <50770385+Java2008@users.noreply.github.com>
2019-06-07 21:02:37 +0300

    Create hesaplama.java

---
+++ .gitignore/hesaplama.java
@@ -0,0 +1,20 @@
+
+public class heaplama {
+	
+
+
+	public static void main (String[]args) {
+		
+		int not=34;
+		
+		if(not>60)
+		
+			System.out.println("Başarılı");
+		else
+			System.out.println("Dersi Geçemediniz");
+			
+			
+	
+		}
+
+}
