8df9b251f1ec1c3b9850c619bf92c1dda53356d6
Java2008 <50770385+Java2008@users.noreply.github.com>
2019-06-07 21:05:56 +0300

    Create yorumlu-işlem.java

---
+++ .gitignore/yorumlu-işlem.java
@@ -0,0 +1,18 @@
+public class yorumlu-işlem {
+	
+	public static void main(String[] args) {
+		
+		
+		int a=2500;
+		
+		
+		int b=5000;
+		
+		
+		int c=18000;
+		System.out.println(c + b + a + " = Bu İşlem Basiti");
+	}
+	
+	
+
+}
