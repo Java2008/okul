f1c44d12cb5a0e379774c400718d16e56a1a2a86
Java2008 <50770385+Java2008@users.noreply.github.com>
2019-06-07 20:06:44 +0300

    Create ifelseif.java

---
+++ .gitignore/ifelseif.java
@@ -0,0 +1,17 @@
+
+public class ifelseif {
+
+	public static void main(String[]args) {
+		
+		int not=60;
+		if(not>85)
+			System.out.println("Harf Notu AA");
+		else if (not<85 && not>=70)
+	        System.out.println("Harf Notu BA");
+		else if (not<70 && not>=60)
+			System.out.println("Harf Notu BB");
+			
+	}
+	
+	
+}
