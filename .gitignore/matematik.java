3de1976b6247623259dab6c0e6fe6d00fe039d93
Java2008 <50770385+Java2008@users.noreply.github.com>
2019-06-07 18:20:15 +0300

    Create matematik.java

---
+++ .gitignore/matematik.java
@@ -0,0 +1,34 @@
+
+public class matematik {
+ 
+	public static void main(String[] args) {
+		
+		byte a=5;
+		byte b=125;
+		System.out.println(a + b);
+		
+		int sayi=1453;
+		System.out.println(sayi);
+		
+		long y=300;
+		long z=y*y;
+		System.out.println(z);
+		
+		double sayi1=5;
+		double sayi2=2;
+		System.out.println(sayi2);
+		
+		char deger1='B';
+		char deger2=',';
+		char deger3='3';
+		System.out.println(deger1+" "+deger2+" "+deger3);
+		
+		int r=6;
+		int k=5;
+		boolean x=(r==k);
+		System.out.println(x);
+				
+		
+	}
+
+}
